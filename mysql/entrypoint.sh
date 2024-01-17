1371a58bd1a8ad49ac134a87edaf90d91f4b37e2
kkrolikowski <kkrolikowski.1993@gmail.com>
2024-01-17 18:24:48 +0100

    fix: docker mysql initialization

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 set -e
-rm -rf /var/lib/mysql/redo_logs/*
+
 # Run mysqldump and load optimized dump only on first run
 if [ ! -e /var/lib/mysql/.initialized ]; then
   # Perform mysqldump to generate optimized dump file
-  mysqldump -h db > /docker-entrypoint-initdb.d/dump.sql
+  mysqldump -h database > /docker-entrypoint-initdb.d/init.sql
 
   # Mark initialization as complete
   touch /var/lib/mysql/.initialized
 fi
 
+mysql -h database < /docker-entrypoint-initdb.d/init.sql
+
 # Start the MySQL server
-exec /entrypoint.sh "$@"
+exec "$@"
