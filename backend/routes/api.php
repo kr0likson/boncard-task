e3457ee8557645502c18390e437b815f34665555
kkrolikowski <kkrolikowski.1993@gmail.com>
2024-01-17 01:50:12 +0100

    feat: add api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('cards', CardController::class)->except(['create', 'index', 'edit']);
+});
+Route::resource('cards', CardController::class)->only('index');
+
