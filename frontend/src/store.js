d2b78bdeb490e6c3d125e6caf00e8e3b10599b23
kkrolikowski <kkrolikowski.1993@gmail.com>
2024-01-17 01:49:39 +0100

    feat: store js for login/registration

---
+++ frontend/src/store.js
@@ -0,0 +1,44 @@
+// store.js
+import {createStore} from 'vuex';
+import axios from 'axios'
+export default createStore({
+    state: {
+        isLoggedIn: false,
+        user: null,
+    },
+    mutations: {
+        setLoggedIn(state, payload) {
+            state.isLoggedIn = payload;
+        },
+        setUser(state, payload) {
+            state.user = payload;
+        },
+    },
+    actions: {
+        async login({commit}, userData) {
+            try {
+                const response = await axios.post('/api/login', userData);
+                commit('setLoggedIn', true);
+                commit('setUser', response.data.user);
+                return response.data;
+            } catch (error) {
+                console.error('Login failed:', error.response.data);
+                throw error;
+            }
+        },
+        async logout({commit}, userData) {
+            const response = await  axios.post('/api/logout', userData);
+            commit('setLoggedIn', false);
+            commit('setUser', null);
+        },
+        async register({commit}, userData) {
+            try {
+                const response = await axios.post('/api/register', userData);
+                console.log('Registration successful:', response);
+                return response.data;
+            } catch (error) {
+                console.error('Registration failed:', error.response || error);
+            }
+        },
+    }
+});
